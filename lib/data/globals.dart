49a4def36071bf18c4a94ed8194bbc184a9dfa33
videcom <chrisj@videcom.com>
2021-05-27 14:17:57 +0200

    fix images

--- lib/data/globals.dart
+++ lib/data/globals.dart
@@ -9,7 +9,7 @@ import 'package:vmba/data/settings.dart';
 String gblAppTitle ;
 String gblLanguage = 'en' ;
 String gblLanguages ;
-String gblBuildFlavor = 'LM';
+String gblBuildFlavor = 'FL';
 String gblError = '';
 String gblSine = '';
 String gblVersion = '';
