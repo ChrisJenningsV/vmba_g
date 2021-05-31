f5e3d40427f57911ac492bff9a4d4c9b4a6d56ef
videcom <chrisj@videcom.com>
2021-05-31 14:06:22 +0200

    put back ios

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/chrisjennings/Documents/dev/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/chrisjennings/Documents/Mobile App/vmba"
+export "FLUTTER_TARGET=/Users/chrisjennings/Documents/Mobile App/vmba/lib/main_fl.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.8"
+export "FLUTTER_BUILD_NUMBER=5"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/chrisjennings/Documents/Mobile App/vmba/.dart_tool/package_config.json"
