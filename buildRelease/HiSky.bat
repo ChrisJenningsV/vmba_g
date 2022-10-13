559412c352320b12007a7d1f8a6978e26f5500d1
chrisj <chrisj"videcom.com>
2022-10-13 11:59:19 +0100

    air swift build

---
+++ buildRelease/HiSky.bat
@@ -0,0 +1,19 @@
+rem build release version of HiSky for android
+@ECHO OFF
+cls
+ECHO =================
+ECHO Build HiSky
+ECHO =================
+ECHO copy key
+copy android\app\hiSkyKey\*.jks android\app\
+ECHO copy bg
+copy lib\assets\hisky\images\bg.png lib\assets\images
+ECHO build store images
+call flutter pub run --no-sound-null-safety flutter_launcher_icons:main -f launch_icons_hisky.yaml
+ECHO build bundle
+call flutter build appbundle -t lib/main.dart --flavor hisky
+
+
+
+
+
